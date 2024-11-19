06438649f2d95a7a7e0c380e72f6be833f5c5a98
fergsosa <134444526+fergsosa@users.noreply.github.com>
2024-11-19 13:28:12 -0300

    Fix: correcciones de CSS

--- js/themeDark-02.js
+++ js/themeDark-02.js
@@ -1,26 +1,21 @@
 const d = document;
 
 export default function ThemeDark(btnLigth, btnDark) {
-  // * selector de botones
   const $btnModeLight = d.querySelector(btnLigth),
     $btnModeDark = d.querySelector(btnDark);
 
-  // * al click se active el tema
-  $btnModeLight.addEventListener("click", setModeDark);
-  $btnModeDark.addEventListener("click", setModeLight);
-
-  // * función para activar
-  function setModeDark() {
-    setUserTheme("dark");
-  }
-  function setModeLight() {
-    setUserTheme("light");
-  }
-
-  // * cambia si es light o dark
-  function setUserTheme(newTheme) {
+  //* Cambia si es light o dark
+  const setUserTheme = (newTheme) => {
     d.documentElement.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
-  }
+  };
+
+  //* Activar tema
+  const setModeDark = () => setUserTheme("dark");
+  const setModeLight = () => setUserTheme("light");
+
+  //* Eventos
+  $btnModeLight.addEventListener("click", setModeDark);
+  $btnModeDark.addEventListener("click", setModeLight);
 }
 //*
